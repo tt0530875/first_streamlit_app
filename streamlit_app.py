1d314c7d40e2c1cb139cc59a5fce66689b7ab78e
tt0530875 <146778786+tt0530875@users.noreply.github.com>
2023-10-03 22:06:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title("My first app - Title")
 streamlit.header("My second app - Header")
 streamlit.text("My third app - Text")
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
